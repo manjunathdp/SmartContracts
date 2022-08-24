91649cad28809b6a12c6138475de8c3179b52dcc
manjunathdp <manjunathdtc3@gmail.com>
2022-08-25 01:17:48 +0530

    program 1

---
+++ Practice program.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.11;
+contract myfile
+{
+    //string public mystring="Hello World";
+    //int public myUint=0;
+    //bool myBool;
+    /*function setMyUint(uint _myUint) public{
+        myUint=_myUint;
+        if(myUint==10){
+            mystring="hi";
+        }
+        else if(myUint==20){
+            mystring="hello";
+        }
+        else
+        {
+            mystring="Bye";
+        }
+       
+    }
+    //boolen function
+    function setBool(bool _myBool) public{
+        if(_myBool){
+            myUint=100;
+        }
+        else{
+            myUint=0;
+        }
+    }*/
+    // increment and decrement function
+    /*function incUint() public{
+        myUint++;
+        if(myUint==10)
+            myUint=0;
+    }
+    function decUint() public{
+        myUint--;
+        if(myUint<0)
+            myUint=5;
+    }*/
+    /*
+    // Function to fatch Accout Balance in Wei
+    address public Account;
+    function SetAddress(address _Address) public {
+
+        Account=_Address;
+    }
+    function getBalance() public view returns(uint){
+        return Account.balance;
+    }
+    */
+    
+
+
+}
